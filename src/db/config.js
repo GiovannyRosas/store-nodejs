db54ba3b3df954bcc04458bf40db51f744b805ae
GiovannyRosas <giovannyrnm.8@gmail.com>
2022-06-27 20:35:51 -0500

    new deploy

--- src/db/config.js
+++ src/db/config.js
@@ -1,18 +1,15 @@
 const { config } = require('./../config/config')
 
-const USER = encodeURIComponent(config.dbUser)
-const PASSWORD = encodeURIComponent(config.dbPass)
-const URI =
-  process.env.URI ||
-  `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
-
 module.exports = {
   development: {
-    url: URI,
+    url: config.dbUrl,
     dialect: 'postgres'
   },
   production: {
-    url: URI,
-    dialect: 'postgres'
+    url: config.dbUrl,
+    dialect: 'postgres',
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 }
