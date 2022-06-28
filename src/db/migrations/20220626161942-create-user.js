e026ec89bc3bc83723208eccb7ea71a26ca1b344
GiovannyRosas <giovannyrnm.8@gmail.com>
2022-06-27 21:08:08 -0500

    fix migration

--- src/db/migrations/20220626161942-create-user.js
+++ src/db/migrations/20220626161942-create-user.js
@@ -1,13 +1,34 @@
 'use strict'
 
-const {
-  UserSchema,
-  USER_TABLE
-} = require('./../models/user.model')
+const { DataTypes, Sequelize } = require('sequelize')
+
+const { USER_TABLE } = require('./../models/user.model')
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(USER_TABLE, UserSchema)
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: 'create_at',
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+      }
+    })
   },
 
   async down(queryInterface) {
